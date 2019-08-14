cd5728c91a990e86d7dcefbcb82da0fb5b1fb8cf
Mikhail <mikhail.yaitsov@arloid.com>
2019-08-14 17:51:51 +0300

    added menu

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import random
 
@@ -20,6 +19,57 @@ class Game:
     def set_title(self):
         pygame.display.set_caption('Snake')
 
+    def text_renderer(self, message, text_color, text_size):
+        font = pygame.font.SysFont('Ubuntu Mono', text_size)
+        text = font.render(message, 0, text_color)
+        return text
+
+    def main_menu(self):
+        menu = True
+        selected = 'Start'
+
+        while menu:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        selected = 'Start'
+                    elif event.key == pygame.K_DOWN:
+                        selected = 'Quit'
+                    if event.key == pygame.K_RETURN:
+                        if selected == 'Start':
+                            print('Start')
+                            run()
+                        if selected == 'Quit':
+                            pygame.quit()
+                            quit()
+
+            self.surface.fill((98, 48, 98))
+
+            text_title = self.text_renderer('Snake Game', (0, 255, 0), 30)
+
+            if selected == 'Start':
+                text_start = self.text_renderer('Start', (250, 250, 250), 20)
+            else:
+                text_start = self.text_renderer('Start', (0, 0, 0), 20)
+            if selected == 'Quit':
+                text_quit = self.text_renderer('Quit', (250, 250, 250), 20)
+            else:
+                text_quit = self.text_renderer('Quit', (0, 0, 0), 20)
+
+            rect_title = text_title.get_rect()
+            rect_start = text_start.get_rect()
+            rect_quit = text_quit.get_rect()
+
+            self.surface.blit(text_title, (self.screen_width / 2 - (rect_title[2] / 2), 80))
+            self.surface.blit(text_start, (self.screen_width / 2 - (rect_start[2] / 2), 180))
+            self.surface.blit(text_quit, (self.screen_width / 2 - (rect_quit[2] / 2), 220))
+
+            pygame.display.update()
+            game.refresh_screen()
+
     def event_loop(self, change_to):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -32,13 +82,13 @@ class Game:
                 if event.key == pygame.K_UP:
                     change_to = 'UP'
                 elif event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    sys.exit()
+                    game.main_menu()
+
         return change_to
 
     def refresh_screen(self):
         pygame.display.flip()
-        game.fps_controller.tick(16)
+        game.fps_controller.tick(18)
 
     def show_score(self):
         font = pygame.font.SysFont('Ubuntu Mono', 18)
@@ -55,8 +105,7 @@ class Game:
         self.surface.blit(surf, rect)
         pygame.display.flip()
         time.sleep(3)
-        pygame.quit()
-        sys.exit()
+        game.main_menu()
 
 
 class Snake:
@@ -76,13 +125,13 @@ class Snake:
     def crossing_border(self):
         if self.snake_head_pos[0] < 0:
             # TODO set screen width
-            self.snake_head_pos[0] = 500
+            self.snake_head_pos[0] = 490
         elif self.snake_head_pos[0] > 490:
-            self.snake_head_pos[0] = -10
+            self.snake_head_pos[0] = 0
         elif self.snake_head_pos[1] < 0:
-            self.snake_head_pos[1] = 500
+            self.snake_head_pos[1] = 490
         elif self.snake_head_pos[1] > 490:
-            self.snake_head_pos[1] = -10
+            self.snake_head_pos[1] = 0
 
     def change_direction(self):
         if any((self.change_to == 'RIGHT' and not self.direction == 'LEFT',
@@ -130,23 +179,29 @@ class Food:
                                                                self.food_size_x, self.food_size_y))
 
 
+def run():
+    snake = Snake()
+    food = Food(game.screen_width, game.screen_height)
+    game.set_title()
+    game.set_surface()
+
+    while True:
+        snake.change_to = game.event_loop(snake.change_to)
+        snake.change_head_position()
+        snake.change_direction()
+        snake.crossing_border()
+        food.food_pos, game.score = snake.snake_body_mechanism(food.food_pos, game.screen_width, game.screen_height,
+                                                               game.score)
+        snake.draw_snake(game.surface)
+        food.draw_food(game.surface)
+        snake.check_crash(game.game_over)
+
+        game.show_score()
+        game.refresh_screen()
+
+
 pygame.init()
 game = Game()
-snake = Snake()
-food = Food(game.screen_width, game.screen_height)
 game.set_title()
 game.set_surface()
-
-while True:
-    snake.change_to = game.event_loop(snake.change_to)
-    snake.change_head_position()
-    snake.change_direction()
-    food.food_pos, game.score = snake.snake_body_mechanism(food.food_pos, game.screen_width, game.screen_height,
-                                                           game.score)
-    snake.draw_snake(game.surface)
-    snake.crossing_border()
-    food.draw_food(game.surface)
-    snake.check_crash(game.game_over)
-
-    game.show_score()
-    game.refresh_screen()
+game.main_menu()
